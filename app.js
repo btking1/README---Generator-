2a94f888ed2bd4255aebe37a8d66137fccb17e31
Booker King <btking1@my.waketech.edu>
2022-06-08 01:18:53 -0400

    boilerplate

---
+++ app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateReadMe = require('./src/page-template');
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of the project? (Required)'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Give a description of the project?'
+        }
+    ])
+}
