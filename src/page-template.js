36145fa386a657c5cae1825b97f82879cddb0ed3
Booker King <btking1@my.waketech.edu>
2022-06-14 19:06:27 -0400

    add badges 2

--- src/page-template.js
+++ src/page-template.js
@@ -7,7 +7,19 @@ module.exports = templateData => {
     
         return `
 ![LICENSE](https://img.shields.io/github/license/${templateData.github}/${templateData.title}?style=flat-square)
-!c
+[contributors-shield]: https://img.shields.io/github/contributors/${templateData.github}/${templateData.title}?style=for-the-badge
+[contributors-url]: https://github.com/othneildrew/Best-README-Template/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/othneildrew/Best-README-Template.svg?style=for-the-badge
+[forks-url]: https://github.com/othneildrew/Best-README-Template/network/members
+[stars-shield]: https://img.shields.io/github/stars/othneildrew/Best-README-Template.svg?style=for-the-badge
+[stars-url]: https://github.com/othneildrew/Best-README-Template/stargazers
+[issues-shield]: https://img.shields.io/github/issues/othneildrew/Best-README-Template.svg?style=for-the-badge
+[issues-url]: https://github.com/othneildrew/Best-README-Template/issues
+[license-shield]: https://img.shields.io/github/license/othneildrew/Best-README-Template.svg?style=for-the-badge
+[license-url]: https://github.com/othneildrew/Best-README-Template/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/othneildrew
+[product-screenshot]: images/screenshot.png
 # ${templateData.title}
     
 ## Description
